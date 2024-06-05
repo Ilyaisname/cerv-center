1504a6616597512b70b6547620553c87f9412db3
Ilya Bogush <Ilya.Bogush@norbit.ru>
2024-06-06 01:48:04 +0300

    chore: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
