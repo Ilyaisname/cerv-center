06ff84298e0bcd03422eed907b7f3dd247a60494
Ilya Bogush <Ilya.Bogush@norbit.ru>
2024-06-10 23:58:04 +0300

    fix: navbar component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TuiActiveZoneModule} from '@taiga-ui/cdk';
 import {TuiTabsModule} from '@taiga-ui/kit';
 import {TuiDataListModule, TuiHostedDropdownModule, TuiModeModule, TuiSvgModule} from '@taiga-ui/core';
 import {NgForOf, NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {NavigationService} from '../shared/services/navigation.service';
 
 @Component({
   selector: 'app-header',
@@ -15,24 +17,20 @@ import {NgForOf, NgIf} from '@angular/common';
     NgForOf,
     TuiActiveZoneModule,
     NgIf,
-    TuiModeModule
+    TuiModeModule,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  readonly tabs = [
-    'Главная',
-    'О Нас',
-    'Новости',
-    'Каталог услуг',
-  ];
+  readonly tabs = inject(NavigationService).getTabs();
 
-  activeElement = String(this.tabs[0]);
+  activeElement = String(this.tabs[0].name);
 
   get activeItemIndex(): number {
-    return this.tabs.indexOf(this.activeElement);
+    return this.tabs.findIndex(tab => tab.name === this.activeElement) || 0;
   }
 
   onClick(activeElement: string): void {
