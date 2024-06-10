cfa8f11444c237f55d82f1fc03be9d29b0fd8057
Ilya Bogush <Ilya.Bogush@norbit.ru>
2024-06-10 23:10:03 +0300

    fix: footer component

--- src/app/shared/staticData/contacts.ts
+++ src/app/shared/staticData/contacts.ts
@@ -5,5 +5,5 @@ export const contacts: IContacts = {
   phoneNumbers: ['+7‒937‒517‒00‒77', '+7 (8342) 37‒47‒76'],
   address: 'г. Саранск, Проспект Ленина, 34, 1 этаж',
   workTime: '09:00–18:00',
-  workMode: 'Пн-Сб'
+  workMode: 'Понедельник-Суббота'
 }
